a78dc700e26c5296aff2ce20c77c3239140d6400
SadPePe24 <danila.2234@yandex.ru>
2024-02-28 18:39:01 +0300

    Продолжение курса. (пока с нерешенными ошибками)

---
+++ src/main/java/com/kissmepls/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,10 @@
+package com.kissmepls.spring.mvc_hibernate_aop.dao;
+
+import com.kissmepls.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.*;
+
+public interface EmployeeDAO
+{
+    public List<Employee> getAllEmployees();
+}
